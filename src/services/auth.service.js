367f68e375666bc03a74484a4b15a5fcd0d2730b
ines-dai <inesdai59@gmail.com>
2025-04-24 10:48:57 +0200

    uncomment code

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -2,16 +2,9 @@ import { supabase } from '@/services/supabase.js';
 
 export class AuthService {
     static async signUp(payload) {
-        console.log('payload:', payload)
         //#TODO Implement error handling in signup
-        // const { data, error } = await supabase.auth.signUp(payload);
-        // data.user.id
-        // data.user.user_metadata.nickname
-        // data.user.user_metadata.step_goal
-        // return { data, error };
-        const data = null;
-        const error = 'failed to sign up';
-        return {data, error};
+        const { data, error } = await supabase.auth.signUp(payload);
+        return { data, error };
     }
 
     static async logIn(payload) {
