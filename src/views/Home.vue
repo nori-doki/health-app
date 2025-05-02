aa91a7c817ce447353218eecfbdcda5680fc96c0
ines-dai <inesdai59@gmail.com>
2025-05-02 18:47:02 +0200

    feat: move donut to its own atom component

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,19 +3,9 @@
         <div class="home-title">
             <h1>Hello, {{ userData.name }}</h1>
         </div>
-        <div class="home-donut-chart">
-            <Doughnut 
-                id="donut-chart" 
-                :data="data" 
-                :options="options" 
-                data-micron="shake"
-                @click="createDonutChart"
-            />
-            <div class="home-donut-chart-content">
-                <p class="home-donut-chart-content-grade">{{ userData.grade }}%</p>
-                <p class="home-donut-chart-content-comment">{{ gradeComment(userData.grade) }}</p>
-            </div>
-        </div>
+        <BaseDonut 
+            :grade="userData.grade"
+        />
         <Button 
             label="Log Your Day" 
             class="home-form-button" 
@@ -27,72 +17,27 @@
 </template>
 
 <script setup>
-import { Chart as ChartJS, ArcElement } from 'chart.js';
-import { Doughnut } from 'vue-chartjs'
 import { ref, onMounted, computed } from 'vue';
 import WeekOverview from '../components/molecules/weekOverview.vue';
 import { ScoreService } from '@/services/score.service';
 import { useRouter } from 'vue-router';
 import { useCookies } from "vue3-cookies";
+import BaseDonut from '../components/atoms/baseDonut.vue';
 
 const router = useRouter();
 const { cookies } = useCookies();
 
 onMounted(() => {
     getDailyScores();
-    createDonutChart();
 });
 
 // Donut Chart
-ChartJS.register(ArcElement);
 const userData = ref({
     name: 'Nori',
     grade: 81,
 
 });
-let data = ref({
-    datasets: [
-        {
-        backgroundColor: getDonutColorArray(userData.value.grade),
-        data: [100],
-        borderWidth: 0,
-        borderRadius: 10,
-        }
-    ]
-});
-let options = ref({
-    responsive: true,
-    maintainAspectRatio: false,
-    cutout: '80%',
-});
-function createDonutChart() {
-    data.value = {
-        datasets: [
-            {
-            backgroundColor: getDonutColorArray(userData.value.grade),
-            data: [userData.value.grade, 100 - userData.value.grade],
-            }
-        ]
-    };
-    options.value = {
-        responsive: true,
-        maintainAspectRatio: false,
-    };
-    ChartJS.register(ArcElement);
-};
-function getDonutColorArray(grade) {
-    if(!grade) return ['#fcfcfc'];
-    if(grade && grade < 50) return ['#ff0099', '#fcfcfc'];
-    if(50 <= grade  && grade < 80) return ['#fffd00', '#fcfcfc'];
-    if(grade >= 80) return ['#1ffb96', '#fcfcfc'];
-};
-function gradeComment (grade) {
-    if(!grade) return '';
-    if(grade && grade < 50) return 'You can still do it!';
-    if(grade >= 50 && grade < 80) return 'Almost there!';
-    if(grade >= 80  && grade < 95) return 'Great job! Keep going!';
-    if(grade >= 95) return 'Excellent!';
-};
+
 
 function goToForm() {
     router.push('/form')
---
+++ test/components/atoms/baseDonut.spec.js
@@ -0,0 +1,30 @@
+import BaseDonut from '../../../src/components/atoms/baseDonut.vue';
+import { describe, vi, it, expect, beforeEach } from 'vitest';
+import { shallowMount } from '@vue/test-utils';
+
+describe("BaseDonut", () => {
+    let wrapper;
+    beforeEach(() => {
+        wrapper = shallowMount(BaseDonut);
+    });
+
+    it("should return correct donut color", () => {
+        expect(wrapper.vm.getDonutColorArray(undefined)).toEqual(['#fcfcfc'])
+        expect(wrapper.vm.getDonutColorArray(0)).toEqual(['#fcfcfc'])
+        expect(wrapper.vm.getDonutColorArray(49)).toEqual(['#ff0099', '#fcfcfc'])
+        expect(wrapper.vm.getDonutColorArray(60)).toEqual(['#fffd00', '#fcfcfc'])
+        expect(wrapper.vm.getDonutColorArray(79)).toEqual(['#fffd00', '#fcfcfc'])
+        expect(wrapper.vm.getDonutColorArray(80)).toEqual(['#1ffb96', '#fcfcfc'])
+        expect(wrapper.vm.getDonutColorArray(81)).toEqual(['#1ffb96', '#fcfcfc'])
+        expect(wrapper.vm.getDonutColorArray(100)).toEqual(['#1ffb96', '#fcfcfc'])
+    })
+
+    it("should return the correct comment", () => {
+        expect(wrapper.vm.gradeComment(0)).toEqual('');
+        expect(wrapper.vm.gradeComment(undefined)).toEqual('');
+        expect(wrapper.vm.gradeComment(49)).toEqual('You can still do it!');
+        expect(wrapper.vm.gradeComment(60)).toEqual('Almost there!');
+        expect(wrapper.vm.gradeComment(80)).toEqual('Great job! Keep going!');
+        expect(wrapper.vm.gradeComment(95)).toEqual('Excellent!');
+    });
+})
