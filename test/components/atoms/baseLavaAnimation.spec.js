b201743e5d72b4859ca3c0a06c5da765b5bd483c
ines-dai <inesdai59@gmail.com>
2025-08-26 16:47:17 +0200

    test: baseLavaAnimation

--- test/components/atoms/baseLavaAnimation.spec.js
+++ test/components/atoms/baseLavaAnimation.spec.js
@@ -3,17 +3,15 @@ import { nextTick } from 'vue'
 import BaseLavaAnimation from '@/components/atoms/baseLavaAnimation.vue';
 
 describe('BaseLavaAnimation', () => {
-  it.todo('mounts properly and creates 80 bubbles', async () => {
+  it('mounts properly and creates 80 bubbles', async () => {
     const wrapper = mount(BaseLavaAnimation);
 
-    
     const bubblesContainer = wrapper.element.querySelector('.bubbles');
     expect(bubblesContainer).not.toBeNull();
-    
-    await nextTick();
-    
-    const bubbles = bubblesContainer.querySelectorAll('.bubble');
-    // expect(bubbles.length).toBe(80);
+    expect(bubblesContainer.children.length).toBe(80);
+    const bubbles = wrapper.findAll('.bubble');
+    console.log('bubbles:', bubbles)
+    expect(bubbles.length).toBe(80);
 
     // const style = bubbles[0].getAttribute('style');
     // expect(style).toContain('--size:');
